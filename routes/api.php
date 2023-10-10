1f4f90393be7c827b165fed154e8647debf426c4
DenDims <dimasp95@icloud.com>
2023-10-10 16:30:13 +0800

    setup project & API get method

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,8 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/posts');
+Route::get('/posts', [PostController::class, 'index']);
 
