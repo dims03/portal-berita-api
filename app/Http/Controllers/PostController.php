c898c6d9973268e5e31f85295afdb5f2a41d5e1d
DenDims <dimasp95@icloud.com>
2023-10-18 14:20:32 +0800

    store data

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -6,14 +6,15 @@ use App\Http\Resources\PostDetailResource;
 use App\Http\Resources\PostResource;
 use App\Models\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class PostController extends Controller
 {
     public function index() {
-        $posts = Post::all();
+        $posts = Post::with('writer:id,username')->get();
         // return response()->json(['data' => $posts]);
 
-        return PostResource::collection($posts);
+        return PostDetailResource::collection($posts);
     }
 
     public function show($id){
@@ -25,5 +26,17 @@ class PostController extends Controller
     public function show2($id){
         $post = Post::findOrFail($id);
         return new PostDetailResource($post);
+    }
+
+    public function store(Request $request){
+
+        $validated = $request->validate([
+            'title' => 'required|max:255',
+            'news_content' => 'required',
+        ]);
+
+        $request['author'] = Auth::user()->id;
+        $post = Post::create($request->all());
+        return new PostDetailResource($post->loadMissing('writer:id,username'));
     }
 }
